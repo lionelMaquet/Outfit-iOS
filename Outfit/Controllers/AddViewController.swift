aa295f1ab68406caf31b9dfa7256a00922360b49
Lionel <lionel.maquet@gmail.com>
2020-09-09 15:56:47 +0200

    Creates and links all vc files

---
+++ Outfit/Controllers/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  Outfit
+//
+//  Created by Lionel Maquet on 09/09/2020.
+//  Copyright © 2020 Lionel Maquet. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Outfit/Controllers/StylesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StylesViewController.swift
+//  Outfit
+//
+//  Created by Lionel Maquet on 09/09/2020.
+//  Copyright © 2020 Lionel Maquet. All rights reserved.
+//
+
+import UIKit
+
+class StylesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
